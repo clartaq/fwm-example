83c0d296fe4f73209af5697fe55352cdd9bf341a
clartaq <clartaq@axaluna.com>
2019-12-11 14:05:29 -0500

    Time and timer task utility functions.

---
+++ src/fwm_example/date_time.clj
@@ -0,0 +1,10 @@
+(ns fwm-example.date-time
+  (:import (java.time LocalTime)
+           (java.time.format DateTimeFormatter)))
+
+(def time-formatter (DateTimeFormatter/ofPattern "h:mm:ss a"))
+
+(defn formatted-now
+  "Return a string with the current local time."
+  []
+  (.format (LocalTime/now) time-formatter))
